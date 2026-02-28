5383bcabf23b90d68b480be55d067d5f812c6111
MarvinWack <marc.uhlherr@gmail.com>
2026-02-28 08:24:04 +0100

    questlog animation

--- Cod-Main/Assets/Scripts/UI/Questlog.cs
+++ Cod-Main/Assets/Scripts/UI/Questlog.cs
@@ -8,8 +8,21 @@ namespace UI
     {
         [SerializeField] private float countdownUntilShow;
         [SerializeField] private float showDuration;
+        [SerializeField] private float animationDuration;
         [SerializeField] private Image logImage;
+        [SerializeField] private RectTransform logRectTransform;
+        private Vector2 originalAnchoredPosition;
+        private Vector2 offscreenAnchoredPosition;
         
+        private void Awake()
+        {
+            if (logRectTransform == null)
+                logRectTransform = logImage.GetComponent<RectTransform>();
+            originalAnchoredPosition = logRectTransform.anchoredPosition;
+            offscreenAnchoredPosition = new Vector2(originalAnchoredPosition.x, originalAnchoredPosition.y + logRectTransform.rect.height + 100f);
+            logRectTransform.anchoredPosition = offscreenAnchoredPosition;
+        }
+
         private void Start()
         {
             StartCoroutine(ShowQuestLog());
@@ -18,9 +31,31 @@ namespace UI
         private IEnumerator ShowQuestLog()
         {
             yield return StartCoroutine(StartCountdown(countdownUntilShow));
-            SetQuestLogVisible(true);
+            yield return StartCoroutine(AnimateTransition(true));
             yield return StartCoroutine(StartCountdown(showDuration));
-            SetQuestLogVisible(false);
+            yield return StartCoroutine(AnimateTransition(false));
+        }
+
+        private IEnumerator AnimateTransition(bool show)
+        {
+            float elapsed = 0f;
+            Vector2 startPos = show ? offscreenAnchoredPosition : originalAnchoredPosition;
+            Vector2 endPos = show ? originalAnchoredPosition : offscreenAnchoredPosition;
+
+            if (show)
+                logImage.enabled = true;
+
+            while (elapsed < animationDuration)
+            {
+                elapsed += Time.deltaTime;
+                float t = Mathf.Clamp01(elapsed / animationDuration);
+                logRectTransform.anchoredPosition = Vector2.Lerp(startPos, endPos, t);
+                yield return null;
+            }
+            logRectTransform.anchoredPosition = endPos;
+
+            if (!show)
+                logImage.enabled = false;
         }
 
         private void SetQuestLogVisible(bool visible)
