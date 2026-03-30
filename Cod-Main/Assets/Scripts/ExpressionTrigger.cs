e13091ad807c27b0c36ed14e419268d29ef903b7
MarvinWack <marc.uhlherr@gmail.com>
2026-03-30 17:01:27 +0200

    adding markers for all characters

--- Cod-Main/Assets/Scripts/ExpressionTrigger.cs
+++ Cod-Main/Assets/Scripts/ExpressionTrigger.cs
@@ -1,4 +1,5 @@
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 using UnityEngine;
 
 namespace DefaultNamespace
@@ -7,11 +8,13 @@ namespace DefaultNamespace
     {
         [SerializeField] private Animator animator;
         [SerializeField] private MarkerManager markerManager;
+        [SerializeField] private string characterName;
 
         private void OnEnable()
         {
             markerManager.OnMarkerReached += TriggerExpression;
             AudioClipPlayer.FinishedPlaying += ResetExpressionToDefault;
+            characterName = name;
         }
 
         private void ResetExpressionToDefault()
@@ -19,15 +22,28 @@ namespace DefaultNamespace
             animator.SetInteger("expState", 1);
         }
 
-        private void TriggerExpression(MarkerManager.MarkerType type)
+        private void TriggerExpression(MarkerManager.MarkerType type, string characterName)
         {
             // Debug.Log("triggered expression: " + type);
-            
             if (type == MarkerManager.MarkerType.Paragraph)
+                return;
+
+            if (!CheckForCharacter(characterName))
                 return;
             
             animator.SetInteger("expState", (int)type);
             
+        }
+
+        private bool CheckForCharacter(string name)
+        {
+            if (name.IsNullOrWhitespace())
+            {
+                Debug.LogError("CharacterName was not set!");
+                return false;
+            }
+
+            return characterName == name;
         }
     }
 }
